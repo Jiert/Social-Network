a1c757bf910c788c54660b0adfbd2ca25924ec09
Jared Easterday <jared.easterday@gmail.com>
2014-04-13 15:27:46 -0700

    getting ready for buildout of social net

--- app.js
+++ app.js
@@ -1,63 +1,16 @@
 var express = require('express'),
-    http = require('http'),
     app = express(),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server),
-    catchPhrases = ['Monkey balls', 'Toaster Mittens', 'Slapping Wet Puppies'];
+    env = process.env.NODE_ENV || 'development';
 
 app.set('view engine', 'jade');
-app.set('view options', { layout : true });
-app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/stooges/chat', function(req, res, next){
-  res.render('chat');
+if (env === 'development') {
+   // configure stuff here
+}
+
+app.get('/', function(req, res){
+  res.render('index.jade', { layout: false });
 });
 
-io.sockets.on('connection', function(socket){
-  var sendChat = function(title, text){
-    socket.emit('chat', {
-      title: title,
-      contents: text
-    });
-  };
-
-  setInterval(function(){
-    var randomIndex = Math.floor(Math.random() * catchPhrases.length);
-    
-    sendChat('Stooge', catchPhrases[randomIndex]);
-  }, 5000);
-
-  sendChat('Welcome to Stooge Chat', 'Sthe Stooges are on line');
-
-  socket.on('chat', function(data){
-    sendChat('You', data.text);
-  });
-
-});
-
-app.get('/stooges/:name?', function(req, res, next){
-  var name = req.params.name;
-
-  switch( name? name.toLowerCase() : ''){
-    case 'larry':
-    case 'curly':
-    case 'moe':
-      res.render('stooges', {stooge: name});
-      break;
-
-    default:
-      next();
-  }
-});
-
-app.get('/stooges/*?', function(req, res){
-  res.render('stooges', {stooge : null});
-});
-
-app.get('/?', function(req, res){
-  res.render('index');
-});
-
-var port = 8080;
-server.listen(port);
-console.log('Listening on port ' + port);
+app.listen(8080);
