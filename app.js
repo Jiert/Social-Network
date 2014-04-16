2d27ae4f4659810de630ca7f501a165213c09936
Jared Easterday <jared.easterday@gmail.com>
2014-04-16 16:35:30 -0700

    added authentication handler

--- app.js
+++ app.js
@@ -13,5 +13,14 @@ app.get('/', function(req, res){
   res.render('index.jade', { layout: false });
 });
 
+app.get('/account/authenticated', function(req, res){
+  if (req.session.loggedIn){
+    res.send(200);
+  }
+  else {
+    res.send(401);
+  }
+});
+
 app.listen(8080);
 console.log('Application Started');
