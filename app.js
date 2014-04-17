a843af28b33553f48b3294b03ff7dcd7086dfaae
Jared Easterday <jared.easterday@gmail.com>
2014-04-17 16:32:09 -0700

    added register handler

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ if (env === 'development') {
    // configure stuff here
 }
 
+app.post('/register', function(req, res){
+  var firstName = req.param('firstName', '');
+  var lastName = req.param('lastName', '');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if (email === null || password === null ){
+    res.send(400);
+    return;
+  }
+
+  Account.register(email, password, firstName, lastName);
+  res.send(200);
+});
+
 app.get('/', function(req, res){
   res.render('index.jade', { layout: false });
 });
