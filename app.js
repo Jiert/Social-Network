71b47e7f5173778dcdcfd453d7ca262347ceb534
Jared Easterday <jared.easterday@gmail.com>
2014-04-17 18:28:39 -0700

    added login handler to app.js

--- app.js
+++ app.js
@@ -24,6 +24,28 @@ app.post('/register', function(req, res){
   res.send(200);
 });
 
+app.post('/login', function(req, res){
+  console.log('login request');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if (email === null || email.length < 1 || password === null || password.length < 1){
+    res.send(400);
+    return;
+  }
+
+  Account.login(email, password, function(success){
+    if (!success){
+      res.send(401);
+      return;
+    }
+    console.log('login was successful');
+    res.send(200);
+  });
+
+});
+
+
 app.get('/', function(req, res){
   res.render('index.jade', { layout: false });
 });
