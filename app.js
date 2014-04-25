b86c5ad8abcaeb1ead5243f26997ccf0f0a9b4da
Jared Easterday <jared.easterday@gmail.com>
2014-04-24 18:32:45 -0700

    Formatting

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.get('/', function(req, res){
 
 app.post('/login', function(req, res){
   console.log('login request');
-  var email = req.param('email', null);
-  var password = req.param('password', null);
+  var email = req.param('email', null),
+      password = req.param('password', null);
 
   if (email === null || email.length < 1 || password === null || password.length < 1){
     res.send(400);
@@ -55,10 +55,10 @@ app.post('/login', function(req, res){
 });
 
 app.post('/register', function(req, res){
-  var firstName = req.param('firstName', '');
-  var lastName = req.param('lastName', '');
-  var email = req.param('email', null);
-  var password = req.param('password', null);
+  var firstName = req.param('firstName', ''),
+      lastName = req.param('lastName', ''),
+      email = req.param('email', null),
+      password = req.param('password', null);
 
   if (email === null || email.length < 1 || password === null || password.length < 1 ){
     res.send(400);
@@ -78,11 +78,11 @@ app.get('/account/authenticated', function(req, res){
   }
 });
 
-
 app.post('/forgotpassword', function(req, res){
-  var hostname = req.headers.host;
-  var resetPasswordUrl = 'http://' + hostname + '/resetPassword';
-  var email = req.param('email', null);
+  var hostname = req.headers.host,
+      resetPasswordUrl = 'http://' + hostname + '/resetPassword',
+      email = req.param('email', null);
+
   if (email === null || email.length < 1){
     res.send(400);
     return;
@@ -106,17 +106,13 @@ app.get('/resetPassword', function(req, res){
 });
 
 app.post('/resetPassword', function(req, res){
-  var accountId = req.param('accountId', null);
-  var password = req.param('password', null);
+  var accountId = req.param('accountId', null),
+      password = req.param('password', null);
   if (accountId !== null && password !== null){
     Account.changeePassword(accountId, password);
   }
   res.render('resetPasswordSuccess.jade');
 });
-
-
-
-
 
 app.listen(8080);
 console.log('Application Started');
