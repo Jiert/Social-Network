084200ac95b9f3c960bc6a3cf988f9c0f1177afd
Jared Easterday <jared.easterday@gmail.com>
2014-04-24 18:30:27 -0700

    rounding out app.js after chapter 6

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 var express = require('express'),
     app = express(),
-    env = process.env.NODE_ENV || 'development';
+    nodemailer = require('nodemailer'),
+    MemoryStore = require('connect').session.MemoryStore,
+    env = process.env.NODE_ENV || 'development',
 
-app.set('view engine', 'jade');
-app.use(express.static(__dirname + '/public'));
+    // Import the data layer
+    mongoose = require('mongoose'),
+    config = { mail: require('./config/mail')},
+
+    // Import the accounts
+    Account = require('./models/Account')(config, mongoose, nodemailer);
 
 if (env === 'development') {
    // configure stuff here
 }
 
-app.post('/register', function(req, res){
-  var firstName = req.param('firstName', '');
-  var lastName = req.param('lastName', '');
-  var email = req.param('email', null);
-  var password = req.param('password', null);
+app.configure(function(){
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.limit('1mb'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: 'SocialNet secret key',
+    store : new MemoryStore()
+  }));
 
-  if (email === null || password === null ){
-    res.send(400);
-    return;
-  }
+  mongoose.connect('mongod://localhost/nodebackbone');
+});
 
-  Account.register(email, password, firstName, lastName);
-  res.send(200);
+app.get('/', function(req, res){
+  res.render('index.jade', { layout: false });
 });
 
 app.post('/login', function(req, res){
@@ -44,6 +53,31 @@ app.post('/login', function(req, res){
   });
 
 });
+
+app.post('/register', function(req, res){
+  var firstName = req.param('firstName', '');
+  var lastName = req.param('lastName', '');
+  var email = req.param('email', null);
+  var password = req.param('password', null);
+
+  if (email === null || email.length < 1 || password === null || password.length < 1 ){
+    res.send(400);
+    return;
+  }
+
+  Account.register(email, password, firstName, lastName);
+  res.send(200);
+});
+
+app.get('/account/authenticated', function(req, res){
+  if (req.session.loggedIn){
+    res.send(200);
+  }
+  else {
+    res.send(401);
+  }
+});
+
 
 app.post('/forgotpassword', function(req, res){
   var hostname = req.headers.host;
@@ -80,18 +114,9 @@ app.post('/resetPassword', function(req, res){
   res.render('resetPasswordSuccess.jade');
 });
 
-app.get('/', function(req, res){
-  res.render('index.jade', { layout: false });
-});
 
-app.get('/account/authenticated', function(req, res){
-  if (req.session.loggedIn){
-    res.send(200);
-  }
-  else {
-    res.send(401);
-  }
-});
+
+
 
 app.listen(8080);
 console.log('Application Started');
